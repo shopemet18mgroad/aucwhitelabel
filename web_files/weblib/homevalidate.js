c36b141685efe16de93054ee2e5a160831be0f8d
preeti <preeti@shopemet.com>
2020-11-18 14:04:57 +0530

    home-page validation wednesday preeti

--- web_files/weblib/homevalidate.js
+++ web_files/weblib/homevalidate.js
@@ -1,23 +1,27 @@
 
 function userid4(){
-var email = document.getElementById("email").value;	
-//var pswd = document.getElementById("pswd").value;	
-var x=document.myform.email.value;  
-var atposition=x.indexOf("@");  
-var dotposition=x.lastIndexOf(".");  
+var exampleInputEmail = document.getElementById("exampleInputEmail").value;	
+var exampleInputPassword = document.getElementById("exampleInputPassword").value;	
+//var x=document.myform.exampleInputEmail.value;  
+//var atposition=x.indexOf("@");  
+// dotposition=x.lastIndexOf(".");  
 
-
-if( $('input[name="ltype[]"]:checked').length == 0 )
+if(exampleInputEmail == ''  || exampleInputPassword == '' ){
+		swal("Alert!",  "Username and Password cannot be blank!", "error");
+		return false;
+	}
+	
+if( $('input[name="ltype"]:checked').length == 0 )
 		   
 			{
 				swal("Alert!",  "Please select at least one option Auctioner Or Bidder", "error");
 				return false;
 			}
 
-if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
+/* if (atposition<1 || dotposition<atposition+2 || dotposition+2>=x.length){  
   swal("Alert!","Please enter a valid User Name, It should contain @ and .  ", "error");  
   return false;
-  }  			
+  }   */			
 
 /* if( pswd.length == 0 )
 		   
