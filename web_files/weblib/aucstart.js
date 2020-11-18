0b3e40b644d2ecc67e42f30356a02ed25482aaa9
nirmaljose <nirmaljose3d@gmail.com>
2020-11-18 13:40:07 +0530

    adlot validation,01 yes or no

--- web_files/weblib/aucstart.js
+++ web_files/weblib/aucstart.js
@@ -129,7 +129,73 @@ function validatestart(){
 	
 	}
           
-function validatestart_edit(){
+	
+			
+	
+
+$('input[id="spcbcertificate2[]"]').on('change', function() {
+    $('input[id="spcbcertificate1[]"]').not(this).prop('checked', false);	
+	
+});
+ $('input[id="spcbcertificate1[]"]').on('change', function() {
+    $('input[id="spcbcertificate2[]"]').not(this).prop('checked', false);	
+	
+});
+
+
+function validatelot(){
+	
+	var slotname = document.getElementById("slotname").value;
+	var scategory = document.getElementById("scategory").value;
+	var sdescription = document.getElementById("sdescription").value;
+	var slotlocation = document.getElementById("slotlocation").value;
+	var semddetail = document.getElementById("semddetail").value;
+	var sprice = document.getElementById("sprice").value;
+	var sqty = document.getElementById("sqty").value;
+	var sunitmeasurment = document.getElementById("sunitmeasurment").value;
+	var sbidbase = document.getElementById("sbidbase").value;
+	var sgst = document.getElementById("sgst").value;
+	var shsncode = document.getElementById("shsncode").value;
+	var sothertax = document.getElementById("sothertax").value;
+	var semdamount = document.getElementById("semdamount").value;
+	var sliftingperiod2 = document.getElementById("sliftingperiod2").value;
+	var sliftingperiod = document.getElementById("sliftingperiod").value;
+	
+	
+	if(slotname == '' || scategory == '' || sdescription == '' || slotlocation == '' ||  semddetail == ''  || sprice == '' || sqty == '' || sunitmeasurment == '' || sbidbase == '' || sgst == '' || shsncode == ''|| sothertax == '' || semdamount == '' || sliftingperiod2 == ''|| sliftingperiod == '' ){
+	
+		swal("Alert!","lotname ,Category, lotlocation, description, frominpectdate&time ,toinpectdate_time, Emddetail,lastdateemdsub,price ,startbidprice, Quantity,Unitmeasurment  ,Bidbase ,gst,othertax,liftingperiod,emdamount,cannot leave any field blank!", "error");
+		return false;
+	}
+		
+
+	 
+		if( $('input[name="spcbcertificate"]:checked').length == 0 )
+		   
+			{
+				swal("Alert!",  "PCB Certificate Should Be Selected", "error");
+				return false;
+			} 
+	}			
+
+	
+
+  function auction_id1(){
+	 //var cat = document.getElementById('scategory').value;
+	  var cat2 = document.getElementById('slotname').value;
+	   var d = new Date();
+	   var m = d.getHours();
+	   var n = d.getMinutes();
+	   var s = d.getSeconds();
+	 
+	 if(cat2.length<6){
+		  document.getElementById('slotno').value = "A-"+m+-+s
+		  ;
+	 }
+ } 
+
+
+/*function validatestart_edit(){
 
 	var svinspection = document.getElementById("svinspection").value;
 	 var sonlineaucdate_time = document.getElementById("sonlineaucdate_time").value;
@@ -189,7 +255,7 @@ function validatestart_edit(){
 				swal("Alert!",  "Please select one checkbox ", "error");
 				return false;
 			} 
-
+ */
 				/* else if(sterms_text == ''){
 		 swal("Alert!", "Please write", "error");
 		 return false;
@@ -211,72 +277,4 @@ function validatestart_edit(){
 	
 
 	
-	}		
-			
-	
-
-$('input[id="spcbcertificate2[]"]').on('change', function() {
-   $('input[id="spcbcertificate1[]"]').not(this).prop('checked', false);	
-	
-});
-$('input[id="spcbcertificate1[]"]').on('change', function() {
-   $('input[id="spcbcertificate2[]"]').not(this).prop('checked', false);	
-	
-});
-
-
-function validatelot(){
-	
-	var slotname = document.getElementById("slotname").value;
-	var scategory = document.getElementById("scategory").value;
-	var sdescription = document.getElementById("sdescription").value;
-	var slotlocation = document.getElementById("slotlocation").value;
-	var sfrominpectdate_time = document.getElementById("sfrominpectdate_time").value;
-	var stoinpectdate_time = document.getElementById("stoinpectdate_time").value;
-	var semddetail = document.getElementById("semddetail").value;
-	var slastdateemdsub = document.getElementById("slastdateemdsub").value;
-	var sprice = document.getElementById("sprice").value;
-	var sstartbidprice = document.getElementById("sstartbidprice").value;
-	var sqty = document.getElementById("sqty").value;
-	var sunitmeasurment = document.getElementById("sunitmeasurment").value;
-	var sbidbase = document.getElementById("sbidbase").value;
-	var sgst = document.getElementById("sgst").value;
-	var shsncode = document.getElementById("shsncode").value;
-	var sothertax = document.getElementById("sothertax").value;
-	var semdamount = document.getElementById("semdamount").value;
-	var sliftingperiod2 = document.getElementById("sliftingperiod2").value;
-	var sliftingperiod = document.getElementById("sliftingperiod").value;
-	
-	
-	if(slotname == '' || scategory == '' || sdescription == '' || slotlocation == '' || sfrominpectdate_time == '' || stoinpectdate_time == '' || semddetail == '' || slastdateemdsub == '' || sprice == '' || sstartbidprice == '' || sqty == '' || sunitmeasurment == '' || sbidbase == '' || sgst == '' || shsncode == ''|| sothertax == '' || semdamount == '' || sliftingperiod2 == ''|| sliftingperiod == '' ){
-	
-		swal("Alert!","lotname ,Category, lotlocation, description, frominpectdate&time ,toinpectdate_time, Emddetail,lastdateemdsub,price ,startbidprice, Quantity,Unitmeasurment  ,Bidbase ,gst,othertax,liftingperiod,emdamount,cannot leave any field blank!", "error");
-		return false;
-	}
-		
-
-	 
-		if( $('input[name="spcbcertificate"]:checked').length == 0 )
-		   
-			{
-				swal("Alert!",  "PCB Certificate Should Be Selected", "error");
-				return false;
-			} 
-	}			
-
-	
-
-  function auction_id1(){
-	 //var cat = document.getElementById('scategory').value;
-	  var cat2 = document.getElementById('slotname').value;
-	   var d = new Date();
-	   var m = d.getHours();
-	   var n = d.getMinutes();
-	   var s = d.getSeconds();
-	 
-	 if(cat2.length<6){
-		  document.getElementById('slotno').value = "A-"+m+-+s
-		  ;
-	 }
- } 
-
+	/* } */	
