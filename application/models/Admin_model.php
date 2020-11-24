48f6b10be4534637c6a2383553a0c32689fc5335
seema <seema27h@gmail.com>
2020-11-24 21:05:10 +0530

    occupied buyer by seema

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -60,7 +60,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			$this->db->select_max('mybid_val');
 			$result = $this->db->get('biddercart')->row();
 			return $result->mybid_val;
-
+		}
 
 	 public function dateclosedauc($table, $date){
 			$this->db->select('*');
@@ -71,29 +71,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			$query = $this->db->get();
 			return $query->result();
 		} 
-		
-		
-		
-		
-		
-		
-		public function maxbidvalue($table,$auctmp, $auclottmp){
-			$this->db->select(max('mybid_val'));
-
-			$this->db->from($table);
-			
-			//$this->db->where('mybid_val >', $compdata);
-			$this->db->where('auctionid =', $auctmp);
-			$this->db->where('lotno =', $auclottmp);
-			$this->db->where('bidderusername =', $sessi);
-			$query = $this->db->get();
-		
-			return $query->result();
-		} 
-		
-			
-		
-	
 		
 		public function gettable($table) { 
 			 $query = $this->db->get($table); 
