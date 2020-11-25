e13aa12b3719e51d7edaa0ce16288902dbcc87f3
seema <seema27h@gmail.com>
2020-11-25 21:03:09 +0530

    startauction by seema

--- application/controllers/Admin_editforthcom.php
+++ application/controllers/Admin_editforthcom.php
@@ -46,7 +46,7 @@ class Admin_editforthcom extends CI_Controller {
 	public function get_table(){
 		$datatoquerydb = $this->uri->segment(3);
 		$this->load->model('Admin_model');
-		$data = $this->Admin_model->get_lookalike('auction','sname',$datatoquerydb);
+		$data = $this->Admin_model->get_lookalike('auction','sauctionid',$datatoquerydb);
 		if(count($data)){
 			echo '<table class="table table-striped table-bordered table-sm text-center mt-5" width="100%" cellspacing="0">';
 			echo '<thead class="bg-warning text-white">';
@@ -74,7 +74,7 @@ class Admin_editforthcom extends CI_Controller {
 			foreach($data as $dat){
 				$uploadfl = unserialize($dat['sterms_condiupload']);
 				echo '<tr>';
-				echo '<td><a href="'.base_url().'admin_editforthcom_2/editforthcom_2/'.$dat['sname'].
+				echo '<td><a href="'.base_url().'admin_editforthcom_2/editforthcom_2/'.str_ireplace('/','-',$dat['sauctionid']).
 				'">';
 				echo $dat['sauctionid'];
 				$passaucid = str_ireplace('/','-',$dat['sauctionid']);
