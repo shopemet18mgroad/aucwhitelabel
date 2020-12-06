c61ec867570249780c465016dccb92601bf0aa7f
Nishant S <nishant6000@gmail.com>
2020-12-06 19:37:28 +0530

    sunday test

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -18,7 +18,8 @@ class Login extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function index()
+
+				public function index()
 	{
 		
 		$this->load->helper('url');
@@ -64,8 +65,7 @@ class Login extends CI_Controller {
 				  $datainserr = "Invalid Password";
 				  header('location: '.base_url().'Login/index_error/'.$datainserr);
 				  die;
-			  }
-			  die;
+			  }  die;
 		}else{
 			$this->load->view('header');
 			$this->load->view('login');
