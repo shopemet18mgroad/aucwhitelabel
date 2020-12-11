fc77d01a39738df6ba2b019d9682fc1a4d71c1ca
seema <seema27h@gmail.com>
2020-12-11 13:58:20 +0530

    header changed by seema in registeration

--- application/views/agreementforseller.php
+++ application/views/agreementforseller.php
@@ -93,7 +93,7 @@ The Contract will remain valid for 1 time sale/ 6 months/ 1 year of this Agreeme
 <form action="./AgreementSellercharg" method="POST">
 10.1	The seller shall be entitled to a service charge of
 <input class="percent" type="text" value="%" name="servperc" maxlength="6" size="4">
-</form>
+
  on the basis of the Delivery Order / sale invoice issued through this auction over the Aucjunction portal.<br>
 10.2	The service charge is exclusive of any other taxes and duties levied by the Statutory Authorities of different States and Central Government. Service Charge will be calculated on the amount realised excluding Excise Duty and Sales Tax<br>
 </p>
@@ -165,7 +165,7 @@ Signature and Seal.
 	</div>
 
 </div>
-
+</form>
 </div>      <!---  body  design  -->
 </div>
 </div>
