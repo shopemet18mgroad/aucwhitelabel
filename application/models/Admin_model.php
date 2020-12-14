5547fedae8a164370ceb731deda2e5e983692e99
Nishant6000 <67912173+Nishant6000@users.noreply.github.com>
2020-12-14 22:56:03 +0530

    Emd approved request will show in auctions

--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -77,6 +77,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			$this->db->where('aucstartdate_time <=', $date);
 			$this->db->where('aucclosedate_time >=', $date);
 			$this->db->where('bidderusername =', $sessi);
+			$this->db->where('emdrequest =', 3);
+			$this->db->or_where('emdrequest =', 1);
 			$query = $this->db->get();
 			return $query->result();
 		}
