762de21828acd8ee7a1a8a1ff161236d66ed0d49
unknown <meghasangmesh2@gmail.com>
2021-03-31 17:11:57 +0530

    sales enquriy3 form  by megha 2

--- application/controllers/Enquiryform_login.php
+++ application/controllers/Enquiryform_login.php
@@ -22,7 +22,8 @@ class Enquiryform_login extends CI_Controller
 	public function index()
 	{
 	$this->load->helper('url');
-	echo $this->uri->segment(3);
+	echo $this->uri->segment(2);
+	echo $this->uri->segment(3);die;
 		if($this->input->post('user')){
 			if($this->input->post('optradio')=="enquiry"){
 				$table = "enquriyform_login";
