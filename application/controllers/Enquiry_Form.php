735b8c6f1ae43883f850f964622ec6f224d12769
Nishant6000 <67912173+Nishant6000@users.noreply.github.com>
2021-04-09 00:55:30 +0530

    app changes9

--- application/controllers/Enquiry_Form.php
+++ application/controllers/Enquiry_Form.php
@@ -66,6 +66,7 @@ class Enquiry_Form extends CI_Controller
 	$location = $this->input->post('location');
 	$latlong = $this->input->post('latlong');
 	$check_db = array('slno' => $uid);
+	print_r($check_db);die;
 		$this->load->model('Admin_model');
 		if($this->Admin_model->check('spocs', $check_db)){
 			$data = array('date'=>$date,'companyname' => $company, 'vaddress' => $address,
@@ -75,10 +76,9 @@ class Enquiry_Form extends CI_Controller
 		'spoc'=>$uid ,'location'=>$location,'website'=>$website,'latlong'=>$latlong);
 		$status = $this->Admin_model->insert('enquiry_form', $data);
 		echo "OK";
-		die;
+		  
 		}else{
 		 echo "INV";
-		 
 	  }
 	die;
 	}
